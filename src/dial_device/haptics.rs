9c0eb48b33dc3341bbc07894b43da0cfd3894b86
Ben Conner <19318632+BenStein1@users.noreply.github.com>
2023-02-15 23:54:24 -0700

    Update haptics.rs
    This part is very un-elegant. I actually used ChatGPT to re-write this section, maintaining all the variables, structs, and functions, but reporting haptics working despite not being connected, as well as doing nothing with any haptics command and just reporting 'OK back to whatever calls it. the iWit has no haptics and was failing when it would try and connect. This bypasses the issue but would kill haptics for the Surface. It also changed the vendor and product id it was looking for to that of the iWit. Changing the Product and vendor id variables to a vector and iterating over them would allow both to be detected, and one could more than likely make case statements for haptics-supported vs. not.

--- src/dial_device/haptics.rs
+++ src/dial_device/haptics.rs
@@ -1,10 +1,7 @@
 use std::sync::mpsc;
-
 use hidapi::{HidApi, HidDevice};
+use crate::error::Result;
 
-use crate::error::{Error, Result};
-
-/// Proxy object - forwards requests to the DialHapticsWorker task
 pub struct DialHaptics {
     msg: mpsc::Sender<DialHapticsWorkerMsg>,
 }
@@ -14,15 +11,11 @@ impl DialHaptics {
         Ok(DialHaptics { msg })
     }
 
-    /// `steps` should be a value between 0 and 3600, which corresponds to the
-    /// number of subdivisions the dial should use.
-    pub fn set_mode(&self, haptics: bool, steps: u16) -> Result<()> {
-        let _ = (self.msg).send(DialHapticsWorkerMsg::SetMode { haptics, steps });
+    pub fn set_mode(&self, _haptics: bool, _steps: u16) -> Result<()> {
         Ok(())
     }
 
-    pub fn buzz(&self, repeat: u8) -> Result<()> {
-        let _ = (self.msg).send(DialHapticsWorkerMsg::Manual { repeat });
+    pub fn buzz(&self, _repeat: u8) -> Result<()> {
         Ok(())
     }
 }
@@ -46,76 +39,29 @@ impl DialHapticsWorker {
 
     pub(super) fn run(&mut self) -> Result<()> {
         loop {
-            eprintln!("haptics worker is waiting...");
-
             loop {
-                match self.msg.recv().unwrap() {
-                    DialHapticsWorkerMsg::DialConnected => break,
-                    other => eprintln!("haptics worker dropped an event: {:?}", other),
-                }
+                self.msg.recv().unwrap();
             }
 
-            eprintln!("haptics worker is ready");
-
-            let api = HidApi::new().map_err(Error::HidError)?;
-            let hid_device = api.open(0x045e, 0x091b).map_err(Error::HidError)?;
-            let wrapper = DialHidWrapper { hid_device };
-
-            loop {
-                match self.msg.recv().unwrap() {
-                    DialHapticsWorkerMsg::DialConnected => {
-                        eprintln!("Unexpected haptics worker ready event.");
-                        // should be fine though?
-                    }
-                    DialHapticsWorkerMsg::DialDisconnected => break,
-                    DialHapticsWorkerMsg::SetMode { haptics, steps } => {
-                        wrapper.set_mode(haptics, steps)?
-                    }
-                    DialHapticsWorkerMsg::Manual { repeat } => wrapper.buzz(repeat)?,
-                }
-            }
+            let _api = HidApi::new().unwrap();
+            let _hid_device = match _api.open(0x2341, 0x484e) {
+                Ok(device) => device,
+                Err(_) => continue,
+            };
         }
     }
 }
 
 struct DialHidWrapper {
-    hid_device: HidDevice,
+    _hid_device: HidDevice,
 }
 
 impl DialHidWrapper {
-    /// `steps` should be a value between 0 and 3600, which corresponds to the
-    /// number of subdivisions the dial should use. If left unspecified, this
-    /// defaults to 36 (an arbitrary choice that "feels good" most of the time)
-    fn set_mode(&self, haptics: bool, steps: u16) -> Result<()> {
-        assert!(steps <= 3600);
-
-        let steps_lo = steps & 0xff;
-        let steps_hi = (steps >> 8) & 0xff;
-
-        let mut buf = [0; 8];
-        buf[0] = 1;
-        buf[1] = steps_lo as u8; // steps
-        buf[2] = steps_hi as u8; // steps
-        buf[3] = 0x00; // Repeat Count
-        buf[4] = if haptics { 0x03 } else { 0x02 }; // auto trigger
-        buf[5] = 0x00; // Waveform Cutoff Time
-        buf[6] = 0x00; // retrigger period
-        buf[7] = 0x00; // retrigger period
-        self.hid_device
-            .send_feature_report(&buf[..8])
-            .map_err(Error::HidError)?;
-
+    fn set_mode(&self, _haptics: bool, _steps: u16) -> Result<()> {
         Ok(())
     }
 
-    fn buzz(&self, repeat: u8) -> Result<()> {
-        let mut buf = [0; 5];
-        buf[0] = 0x01; // Report ID
-        buf[1] = repeat; // RepeatCount
-        buf[2] = 0x03; // ManualTrigger
-        buf[3] = 0x00; // RetriggerPeriod (lo)
-        buf[4] = 0x00; // RetriggerPeriod (hi)
-        self.hid_device.write(&buf).map_err(Error::HidError)?;
+    fn buzz(&self, _repeat: u8) -> Result<()> {
         Ok(())
     }
 }
